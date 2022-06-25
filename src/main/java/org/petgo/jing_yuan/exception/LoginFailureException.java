52e676ff8ed586407b01a28c0e916bbe14e0b869
abbyyvan <499965776@qq.com>
2022-06-25 18:16:11 -0400

    exception

---
+++ src/main/java/org/petgo/jing_yuan/exception/LoginFailureException.java
@@ -0,0 +1,9 @@
+package org.petgo.jing_yuan.exception;
+
+public class LoginFailureException extends Exception {
+
+    public LoginFailureException(String message) {
+        super(message);
+    }
+
+}
