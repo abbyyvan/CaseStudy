c07b1c95086fa50c86254a86525bde7e4b274f85
abbyyvan <499965776@qq.com>
2022-06-24 21:30:09 -0400

    User use lombok

--- Petgo_JingYuan/src/main/java/org/petgo/jing_yuan/model/User.java
+++ Petgo_JingYuan/src/main/java/org/petgo/jing_yuan/model/User.java
@@ -2,15 +2,23 @@ package org.petgo.jing_yuan.model;
 
 import javax.persistence.*;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "User")
-
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(name = "username")
@@ -21,52 +29,5 @@ public class User {
 
     @Column(name = "password")
     private String password;
-
-    public User() {
-    }
-
-    public User(long id, String username, String email, String password) {
-        this.id = id;
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    @Override
-    public String toString() {
-        return "User [email=" + email + ", id=" + id + ", password=" + password + ", username=" + username + "]";
-    }
 
 }
