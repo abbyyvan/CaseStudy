8ca634e37b618cf4f0d84598f7fa65c72f7cd3d8
abbyyvan <499965776@qq.com>
2022-06-24 22:23:05 -0400

    move files to root directory, add .gitignore

---
+++ src/main/java/org/petgo/jing_yuan/model/User.java
@@ -0,0 +1,33 @@
+package org.petgo.jing_yuan.model;
+
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "User")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+}
