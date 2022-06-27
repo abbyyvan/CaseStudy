b4832b0429a11d143906db92217e65902fb0600b
abbyyvan <499965776@qq.com>
2022-06-27 13:51:43 -0400

    use final for constant

--- src/main/java/org/petgo/jing_yuan/model/User.java
+++ src/main/java/org/petgo/jing_yuan/model/User.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Set;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -20,17 +21,23 @@ import lombok.ToString;
 @Entity
 @Table(name = "User")
 public class User {
+    public static final int EMAIL_LENGTH = 56;
+    public static final int USERNAME_LEAST_CHAR = 3;
+    public static final int PASSWORD_LENGTH = 12;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private long id;
 
-    @Column(name = "email")
+    @Column(name = "email", nullable = false, length = EMAIL_LENGTH)
     private String email;
 
-    @Column(name = "password")
+    @Column(name = "password", length = PASSWORD_LENGTH)
     private String password;
+
+    @Size(min = USERNAME_LEAST_CHAR, message = "Username must be at least 3 characters long")
+    private String username;
 
     // one user can have many pet applications
     // @OneToMany(targetEntity = Application.class, cascade = { CascadeType.ALL })
