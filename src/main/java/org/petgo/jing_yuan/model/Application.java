f32479b67ec9f08f0aeb6980f9d980d343274f76
abbyyvan <499965776@qq.com>
2022-06-27 14:00:34 -0400

    update entity

--- src/main/java/org/petgo/jing_yuan/model/Application.java
+++ src/main/java/org/petgo/jing_yuan/model/Application.java
@@ -3,13 +3,16 @@ package org.petgo.jing_yuan.model;
 import lombok.*;
 import javax.persistence.*;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
 //this is the class for adoption application
 //Date 
 //boolean isApproved, false 
-
 
 @Entity
 @Table(name = "application")
@@ -27,13 +30,20 @@ public class Application {
     @JoinColumn(name = "user_id")
     private User user;
 
-
-    //each user can only apply to adopt one pet at a time
+    // each user can only apply to adopt one pet at a time
     @OneToOne
     @JoinColumn(name = "pet_id")
     private Pet pet;
 
+    @Column(name = "created_at", updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 
-    
+    @Column(name = "is_approved")
+    private boolean isApproved;
+
 }
